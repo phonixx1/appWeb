afaabe8b472a25d3ebe9b396a915b1503c34ec00
sacha <revol.1998@outlook.fr>
2021-04-11 00:37:36 +0200

    Premiers graphiques et quelques points design

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.express as px
 from amundiScript import nan, pd, genDrawUpDrawDown
 
 TRIGER = 0.2
@@ -11,14 +12,19 @@ data = pd.read_csv(FILE_PATH, sep=',')
 data['date'] = pd.to_datetime(data['date'],format = '%d/%m/%Y')
 data["returns"] = data["price"].pct_change().fillna(0)
 positiveCumReturn, negativeCumReturn = genDrawUpDrawDown(data["returns"],TRIGER)
+taglist = ['positiveCumReturn' if x is not nan else 'negativeCumReturn' for x in positiveCumReturn]
+sumCumReturns = [x if x is not nan else y for (x,y) in zip(positiveCumReturn, negativeCumReturn)]
+
 data['positiveCumReturn'] = pd.DataFrame(positiveCumReturn)
 data['negativeCumReturn'] = pd.DataFrame(negativeCumReturn)
+data['sumCumReturns'] = pd.DataFrame(sumCumReturns)
+data['taglist'] = pd.DataFrame(taglist)
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div(
     children=[
-        html.H1(children="Draw-up/Draw-down Analytics",),
+        html.H1(children="Draw-up/Draw-down Analytics", style={"fontSize": "48px", "color": "red",'text-align':'center'},),
         html.P(
             children="Analyze of the draw-up and draw-down of cumulative returns"
             " of a time series "
@@ -32,21 +38,22 @@ app.layout = html.Div(
                         "y": data["price"],
                         "type": "lines",
                     },
+                    
                 ],
                 "layout": {"title": "Overview of the price variation"},
             },
         ),
         dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["date"],
-                        "y": data["positiveCumReturn"],
-                        "type": "lines",
-                    },
-                ],
-                "layout": {"title": "positiveCumReturn"},
-            },
+            figure=px.area(data,
+            x="date", y="positiveCumReturn",title="Display of draw-up of cumulatif returns"
+            )
+            
+        ),
+        dcc.Graph(
+            figure=px.area(data,
+            x="date", y="negativeCumReturn",
+            title="Display of draw-down of cumulatif returns"
+            )
         ),
         dcc.Graph(
             figure={
@@ -55,11 +62,32 @@ app.layout = html.Div(
                         "x": data["date"],
                         "y": data["negativeCumReturn"],
                         "type": "lines",
+                        'name': 'draw-down',
+                    },
+                    {
+                        "x": data["date"],
+                        "y": data["positiveCumReturn"],
+                        "type": "lines",
+                        'name': 'draw-up',
                     },
                 ],
-                "layout": {"title": "negativeCumReturn"},
+                "layout": {"title": "Display of draw-down/draw-up of cumulatif returns"},
             },
         ),
+        
+        dcc.Graph(
+        figure=px.area(data,
+            x="date", y="sumCumReturns",color = 'taglist', 
+            line_group="taglist",
+
+                )
+            
+        ),
+
+        
     ]
 )
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
 
